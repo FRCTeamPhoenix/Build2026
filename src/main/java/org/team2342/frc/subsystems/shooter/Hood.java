c8dfb6a687949c164e0d4801a6a1cccaaa78657d
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2026-01-31 16:05:07 -0500

    review

--- src/main/java/org/team2342/frc/subsystems/shooter/Hood.java
+++ src/main/java/org/team2342/frc/subsystems/shooter/Hood.java
@@ -4,7 +4,7 @@
 // This source code is licensed under the MIT License.
 // See the LICENSE file in the root directory of this project.
 
-package org.team2342.frc.subsystems.shooterhood;
+package org.team2342.frc.subsystems.shooter;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Alert;
@@ -16,11 +16,12 @@ import org.team2342.lib.motors.smart.SmartMotorIO;
 import org.team2342.lib.motors.smart.SmartMotorIOInputsAutoLogged;
 import org.team2342.lib.sensors.absolute.AbsoluteEncoderIO;
 import org.team2342.lib.sensors.absolute.AbsoluteEncoderIOInputsAutoLogged;
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 import org.team2342.frc.Constants.HoodConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class ShooterHood extends SubsystemBase {
+public class Hood extends SubsystemBase {
   private final SmartMotorIO motor;
   private final SmartMotorIOInputsAutoLogged motorInputs = new SmartMotorIOInputsAutoLogged();
 
@@ -31,12 +32,11 @@ public class ShooterHood extends SubsystemBase {
   private final Alert motorAlert = new Alert("Hood motor is disconnected!", AlertType.kError);
   private final Alert encoderAlert = new Alert("Hood Encoder is disconnected!", AlertType.kError);
 
-  public ShooterHood(SmartMotorIO motor, AbsoluteEncoderIO encoder) {
+  public Hood(SmartMotorIO motor, AbsoluteEncoderIO encoder) {
     this.motor = motor;
     this.encoder = encoder;
 
-    setName("Shooter Hood");
-    motorInputs.positionRad = 0.0;
+    setName("Shooter/Hood");
     setDefaultCommand(run(() -> motor.runVoltage(0.0)));
   }
 
@@ -48,28 +48,29 @@ public class ShooterHood extends SubsystemBase {
     motorAlert.set(!motorInputs.motorsConnected[0]);
     encoderAlert.set(!encoderInputs.connected);
     
-    Logger.processInputs("Shooter Hood/Motor", motorInputs);
-    Logger.processInputs("Shooter Hood/encoder", encoderInputs);
+    Logger.processInputs("Shooter/Hood/Motor", motorInputs);
+    Logger.processInputs("Shooter/Hood/encoder", encoderInputs);
 
-    ExecutionLogger.log("Shooter Hood");
+    ExecutionLogger.log("Shooter/Hood");
   }
 
+  @AutoLogOutput(key = "Shooter/Hood/Angle")
   public Rotation2d getAngle() {
-    return Rotation2d.fromRadians(motorInputs.positionRad / HoodConstants.GEAR_RATIO);
+    return Rotation2d.fromRadians(motorInputs.positionRad);
   }
 
   public Command goToAngle(Rotation2d targetAngle) {
    return run(() -> motor.runPosition(targetAngle.getRadians()))
-        .until(() -> Math.abs(targetAngle.getRadians() - getAngle().getRadians())
+        .until(() -> Math.abs(targetAngle.minus(getAngle()).getRadians())
                     < HoodConstants.TARGET_TOLERANCE)
-        .withName("Hood Go To Angle");
+        .withName("Hood GoToAngle");
   }
 
   public Command holdAngle(Rotation2d targetAngle) {
-    return run(() -> motor.runPosition(targetAngle.getRadians())).withName("Shooter Hold Angle Hold");
+    return run(() -> motor.runPosition(targetAngle.getRadians())).withName("Shooter HoldAngle");
   }
 
   public Command stop() {
-    return runOnce(() -> motor.runVoltage(0.0)).withName("Shooter Hood Stop");
+    return runOnce(() -> motor.runVoltage(0.0)).withName("Shooter HoodStop");
   }
 }
